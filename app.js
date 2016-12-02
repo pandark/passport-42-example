2eb7e5b3f7bbe877ea6d629a843be11188d6f59a
Adrien Pachkoff <adrien-dev@pachkoff.com>
2016-12-02 22:38:24 +0100

    missing parameter to error handling middleware

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ app.get('/logout', function(req, res){
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
